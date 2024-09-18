35bb9f540089795d90a256aeafe504b463f16c95
Gumiho2504 <hemchanmetrey123@gmail.com>
2024-09-18 08:44:16 +0700

    upgrade

--- src/main/java/com/jobapplication/jobapplication/JobapplicationApplication.java
+++ src/main/java/com/jobapplication/jobapplication/JobapplicationApplication.java
@@ -15,20 +15,20 @@ public class JobapplicationApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(JobapplicationApplication.class, args);
 	}
-	@Bean
-	public CommandLineRunner commandLineRunner(JobRepository appDAO){
-		return runner ->{
+// 	@Bean
+// 	public CommandLineRunner commandLineRunner(JobRepository appDAO){
+// 		return runner ->{
 
 
-			System.out.println("Finding all jobs ... ");
-//			List<Job> jobs = appDAO.findAll();
-//			System.out.println("tempInstructor = " + jobs);
-//			jobs.forEach(j -> System.out.println("the associate course : " + j.getUser()));
-//			System.out.println("the associate course : " );
-//			System.out.println("Done!");
+// 			System.out.println("Finding all jobs ... ");
+// //			List<Job> jobs = appDAO.findAll();
+// //			System.out.println("tempInstructor = " + jobs);
+// //			jobs.forEach(j -> System.out.println("the associate course : " + j.getUser()));
+// //			System.out.println("the associate course : " );
+// //			System.out.println("Done!");
 
-		};
-	}
+// 		};
+// 	}
 
 	void addJob(Job job){
 		
